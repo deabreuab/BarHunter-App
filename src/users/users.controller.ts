7a5e5430e422b6d105375ddbab8f33a7a2c5686e
Barbara <0deabreubarbara@gmail.com>
2025-04-14 11:08:23 -0500

    refactor: remove commented code from UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,6 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 @Controller('users')
 export class UsersController {
-  // 👇 Ruta protegida
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
   getProfile(@Req() req: Request) {
