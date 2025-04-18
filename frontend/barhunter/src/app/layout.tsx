1520867c461828415ad4c1ee3f9a45252d389d30
Barbara De Abreu <0deabreubarbara@gmail.com>
2025-04-18 11:16:37 -0500

    feat: fixing layout

--- frontend/barhunter/src/app/layout.tsx
+++ frontend/barhunter/src/app/layout.tsx
@@ -28,12 +28,12 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="min-h-screen bg-customcolors-darkblue text-white">
           <Header />
-          <div className="container mx-auto px-4 py-6 flex">
+            <div className="m-0 p-0 md:mx-auto md:px-4 md:container md:py-6 flex">
             <Sidebar />
             {/* main content */}
-            <main className="flex-1 p-5 overflow-x-hidden">{children}</main>
+            <main className="flex-1 p-0 md:p-5 overflow-x-hidden">{children}</main>
             <BottomNavbar />
-          </div>
+            </div>
         </div>
       </body>
     </html>
--- frontend/barhunter/src/app/page.tsx
+++ frontend/barhunter/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Card, Slide } from "./components";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-white mb-6">Todas las opciones</h1>
+      <h1 className="text-3xl p-4 font-bold text-white">Todas las opciones</h1>
       <div className="max-w-full overflow-hidden p-4">
         <Slide>
           {[1, 2, 3, 4].map((i) => (
@@ -13,14 +13,17 @@ export default function Home() {
           ))}
         </Slide>
       </div>
-      <h1 className="text-3xl font-bold text-white mb-6">
+      <h1 className="text-3xl p-4 font-bold text-white">
         Opciones en California
       </h1>
-      <Slide>
-        {[1, 2, 3].map((i) => (
-          <Card key={i} />
-        ))}
-      </Slide>
+
+      <div className="max-w-full overflow-hidden p-4">
+        <Slide>
+          {[1, 2, 3].map((i) => (
+            <Card key={i} />
+          ))}
+        </Slide>
+      </div>
     </div>
   );
 }
