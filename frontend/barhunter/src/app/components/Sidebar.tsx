c3d1dcccad74266c3b99f6aaaed15b08aef3cc6d
Barbara De Abreu <0deabreubarbara@gmail.com>
2025-04-18 22:43:52 -0500

    feat: logout function

--- frontend/barhunter/src/app/components/Sidebar.tsx
+++ frontend/barhunter/src/app/components/Sidebar.tsx
@@ -1,37 +1,51 @@
+"use client";
+
 import React from "react";
 import { IoHeartOutline } from "react-icons/io5";
 import { LuCalendar, LuCompass } from "react-icons/lu";
 import { MdLogout } from "react-icons/md";
 import { PiGear } from "react-icons/pi";
 import { RiMap2Line } from "react-icons/ri";
+import { removeAccessToken } from "../utils/localStorage";
+import { useRouter } from "next/navigation";
 
 function Sidebar() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    removeAccessToken();
+    router.push("/login");
+  };
+
   return (
     <div className="hidden md:flex">
       <div className="w-64 bg-customcolors-darkblue border-r border-gray-800 min-h-[calc(100vh-120px)] pr-6">
         <ul className="flex flex-col space-y-6 py-8">
           <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white">
-            <LuCompass className="w-5 h-5"/>
+            <LuCompass className="w-5 h-5" />
             <span>Explorar</span>
           </li>
           <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white">
-            <RiMap2Line className="w-5 h-5"/>
+            <RiMap2Line className="w-5 h-5" />
             <span>Mapa</span>
           </li>
           <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white">
-            <LuCalendar className="w-5 h-5"/>
+            <LuCalendar className="w-5 h-5" />
             <span>Eventos</span>
           </li>
           <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white">
-            <IoHeartOutline className="w-5 h-5"/>
+            <IoHeartOutline className="w-5 h-5" />
             <span>Favoritos</span>
           </li>
           <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white">
-            <PiGear className="w-5 h-5"/>
+            <PiGear className="w-5 h-5" />
             <span>Configuracion</span>
           </li>
-          <li className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white mt-14">
-            <MdLogout className="w-5 h-5"/>
+          <li
+            onClick={handleLogout}
+            className="flex items-center space-x-3 text-gray-400 cursor-pointer hover:text-white mt-14"
+          >
+            <MdLogout className="w-5 h-5" />
             <span>Cerrar Sesion</span>
           </li>
         </ul>
