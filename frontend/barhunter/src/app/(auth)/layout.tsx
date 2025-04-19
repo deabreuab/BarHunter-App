9c85f4d7cf607c6b04c1529f0bae4e18f84f2846
Barbara De Abreu <0deabreubarbara@gmail.com>
2025-04-18 23:15:58 -0500

    feat: protected route

--- frontend/barhunter/src/app/(auth)/layout.tsx
+++ frontend/barhunter/src/app/(auth)/layout.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import withAuth from "../utils/withAuth";
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-customcolors-darkblue text-white">
       {children}
     </div>
   );
 }
+
+export default withAuth(MainLayout as React.ComponentType<any>);
